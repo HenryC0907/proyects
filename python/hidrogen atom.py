9a5263abf7fd33c14b3e60c183d675a955d6cf6c
Henry Cañón <131017857+HenryC0907@users.noreply.github.com>
2024-06-15 15:37:00 -0500

    Update hidrogen atom.py

--- python/hidrogen atom.py
+++ python/hidrogen atom.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import sph_harm, genlaguerre, factorial
+from matplotlib.colors import LinearSegmentedColormap
 
 def R_func(r, l, n, a):  #Parte Radial
     L = genlaguerre(n - l - 1, 2 * l + 1)(r / a)
@@ -18,33 +19,28 @@ def u(r, theta, phi, l, m, n, a):   #\psi(r,\theta,\phi_{0})
 
 
 # Parámetros del átomo de hidrógeno
-n = 3  # Parámetro n para los polinomios de Laguerre /
-l = 1  # Grado del armónico esférico /
+n = 1  # Parámetro n para los polinomios de Laguerre /
+l = 0  # Grado del armónico esférico /
 m = 0  # Orden del armónico esférico / momento magnético
 
-a = 5.29177e-5  # Radio de Bohr
+a = 5.29177e-5  # Radio de Bohr (aprox)
 
-r_max = 5 * a  # Máximo radio a considerar
+r_max = 10 * a  # Máximo radio a considerar
 
-phi_fixed = np.pi / 2  
+phi_fixed = np.pi / 2    #fijar un ángulo para computarlo en "polares"
 
-# Dominio y función
+
 Nr = 100
 Ntheta = 100
 r = np.linspace(0.1 * a, r_max, Nr)  
 theta = np.linspace(0, 2 * np.pi, Ntheta)
 R, Theta = np.meshgrid(r, theta, indexing='ij')
 
-# evaluar la función
 U = u(R, Theta, phi_fixed, l, m, n, a)
-
 
 #Graficar: 
 
-from matplotlib.colors import LinearSegmentedColormap
-
 cmap = LinearSegmentedColormap.from_list('red_to_black', ['black','orange'])
-# Configurar el fondo oscuro
 plt.style.use('dark_background')
 
 fig = plt.figure(figsize=(8, 6))
@@ -52,16 +48,13 @@ ax = fig.add_subplot(111, polar=True)
 levels = 300
 c = ax.contourf(Theta, R / a, U, levels, cmap=cmap)  # Normalizar el radio por el radio de Bohr
 
-# Añadir una barra de color sin etiquetas
 cbar = fig.colorbar(c, ax=ax)
-cbar.set_ticks([])  # Eliminar las etiquetas de la barra de color
+cbar.set_ticks([])  
 
-# Configuración de la gráfica
 ax.set_title(rf'$({n}, \, {l}, \, {m})$', fontsize=20, color='white')
-ax.set_yticklabels([])  # Eliminar las etiquetas radiales
-ax.set_xticklabels([])  # Eliminar las etiquetas angulares
+ax.set_yticklabels([])  
+ax.set_xticklabels([])  
 
-# Cambiar color de los ejes y etiquetas
-ax.tick_params(colors='white', which='both')  # Evitar cualquier etiqueta de tick
+ax.tick_params(colors='white', which='both')  
 
 plt.show()
